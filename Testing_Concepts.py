79244b5fc35829345e389db765378a5008482a65
davidocuevas <davidocuevas01@gmail.com>
2021-09-25 14:59:11 -0700

    Update Testing_Concepts.py

--- Testing_Concepts.py
+++ Testing_Concepts.py
@@ -2,37 +2,95 @@
 # Description: This program uses an artificial recurrent neural network called Long Short Term Memory (LSTM) to predict
 #              the closing price of a Stock, ETF, or Index Fund, using the past 60 day stock price
 
-# Import relevant libraries
 import math
 import pandas_datareader as web
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-#from keras.models import Sequential
-#from keras.layers import Dense, LSTM
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
+
+
 plt.style.use('fivethirtyeight')
 
-# Get the Stock quote & Print data
-df = web.DataReader('IHAK', data_source = 'yahoo', start = '2020-01-01', end = '2021-01-01')
-print(df)
+df = web.DataReader('AAPL', data_source='yahoo', start='2012-01-01', end='2019-12-17')
 
-# Visualize closing price history
+"""""
 plt.figure(figsize=(16,8))
 plt.title('Close Price History')
 plt.plot(df['Close'])
 plt.xlabel('Date', fontsize=18)
 plt.ylabel('Close Price USD ($)', fontsize=18)
 plt.show()
-
-# Create a new dataframe with only the "Close" column
+"""""
 data = df.filter(['Close'])
-
-# Convert the dataframe to a numpy array
 dataset = data.values
+training_data_len = math.ceil(len(dataset) * .8)
+#print(training_data_len)
 
-# Get the number of rows to train the model on
+#Scale the data
+scalar = MinMaxScaler(feature_range=(0,1))
+scaled_data = scalar.fit_transform(dataset)
+#print(scaled_data)
 
+#Create the training data set
+#Create the scaled training data set
+train_data = scaled_data[0:training_data_len , :]
+#split the data into x_train and y_train data sets
+x_train = []
+y_train = []
 
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+    #if i <= 60:
+        #print(x_train)
+        #print(y_train)
+        #print()
 
+#convert the x_train amd y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+#Reshape the x_train dataset to 3D from 2D LSTM expects 3D
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+#print(x_train.shape)
+
+#Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1],1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+test_data = scaled_data[training_data_len - 60:, :]
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+predictions = model.predict(x_test)
+predictions = scalar.inverse_transform(predictions)
+
+rmse = np.sqrt(np.mean(predictions-y_test)**2)
+#print("Bitchin:", rmse)
+
+#Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+
+#Visualize the data
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions',], loc='lower right')
+plt.show()
 
